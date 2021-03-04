386fdab5c8932ac264098fe6ce215974f839110c
Spikey <spikey.f@gmail.com>
2021-03-04 20:32:31 +0100

    Added SDK3 support for 05431 device

---
+++ drivers/05431/device.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Homey = require('homey');
+const { ZwaveDevice } = require('homey-zwavedriver');
+
+// http://devel.pepper1.net/zwavedb/device/18
+class Device_05431 extends ZwaveDevice {
+
+  onNodeInit() {
+    this.registerCapability('onoff', 'SWITCH_BINARY');
+  }
+}
+
+
+module.exports = Device_05431;
