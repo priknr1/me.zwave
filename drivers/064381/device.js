0f1647c181951fab1cc74e74bda7d2423784e735
Spikey <spikey.f@gmail.com>
2021-03-07 19:48:07 +0100

    Added SDK3 support for 064381 socket devices

---
+++ drivers/064381/device.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Homey = require('homey');
+const { ZwaveDevice } = require('homey-zwavedriver');
+
+// http://devel.pepper1.net/zwavedb/device/567
+class Device_064381 extends ZwaveDevice {
+
+  onNodeInit() {
+    this.registerCapability('onoff', 'SWITCH_BINARY');
+    
+    // Origanally it was using SWITCH MITLILEVEL command, but 
+    // acording to the manual it does not support command class
+    //this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
+  }
+}
+
+module.exports = Device_064381;
